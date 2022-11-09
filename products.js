125304093d84af3b20cf2ac614c2584332e43ad2
faisalpinitod <faisalpinitod@gmail.com>
2022-11-09 23:53:16 +0530

    Day-1 issues and task completed

---
+++ products.js
@@ -0,0 +1,227 @@
+let data=[
+    {
+        "createdAt": "2022-11-07T15:15:38.533Z",
+        "tittle": " mCaffeine Naked & Raw Tan Removal",
+        "img": " https://www.netmeds.com/images/product-v1/150x150/959541/mcaffeine_naked_raw_tan_removal_coffee_body_scrub_100_gm_0_0.jpg",
+        "price": 582,
+        "category": " Body Care ",
+        "id": "1"
+    },
+    {
+        "createdAt": "2022-11-08T02:15:54.908Z",
+        "tittle": " Patanjali Dant Kanti Tooth Paste - Natural",
+        "img": " https://www.netmeds.com/images/product-v1/150x150/413967/patanjali_dant_kanti_tooth_paste_natural_200_gm_0.jpg",
+        "price":373,
+        "category": "Body Care",
+        "id": "2"
+    },
+    {
+        "createdAt": "2022-11-08T01:51:37.482Z",
+        "tittle": " Netmeds 3 Ply Face Mask with Nose Pin",
+        "img": " https://www.netmeds.com/images/product-v1/150x150/977197/netmeds_3_ply_face_mask_with_nose_pin_50s_0_1.jpg",
+        "price": 395,
+        "category": "Hair care",
+        "id": "3"
+    },
+    {
+        "createdAt": "2022-11-07T15:49:33.609Z",
+        "tittle": " Truuth Adult Diaper - Strap (L) 10's",
+        "img": " https://www.netmeds.com/images/product-v1/150x150/1042881/truuth_adult_diaper_strap_l_10s_476360_0_0.jpg",
+        "price": 180,
+        "category": "Oral Care",
+        "id": "4"
+    },
+    {
+        "createdAt": "2022-11-08T13:49:06.732Z",
+        "tittle": " Maxi For You Travel Pack Toothbrush - Soft",
+        "img": " https://www.netmeds.com/images/product-v1/150x150/1004097/maxi_travel_pack_toothbrush_buy_1_get_1_free_0_1.jpg",
+        "price": 172,
+        "category": " Oral Care ",
+        "id": "5"
+    },
+    {
+        "createdAt": "2022-11-08T02:34:40.930Z",
+        "tittle": " Shake HandZ-E Alcohol Hand Rub With Flip",
+        "img": " https://www.netmeds.com/images/product-v1/150x150/958677/shake_handz_e_alcohol_hand_rub_with_flip_cap_500_ml_0_1.jpg",
+        "price": 650,
+        "category": " Hair care ",
+        "id": "6"
+    },
+    {
+        "createdAt": "2022-11-07T15:41:48.543Z",
+        "tittle": " Ciphands Antiseptic Hand Sanitizer 100 ml",
+        "img": " https://www.netmeds.com/images/product-v1/150x150/908599/ciphands_antiseptic_hand_sanitizer_100_ml_0_1.jpg",
+        "price": 96,
+        "category": " Oral Care ",
+        "id": "7"
+    },
+    {
+        "createdAt": "2022-11-08T05:44:58.393Z",
+        "tittle": " Patanjali Dant Kanti Tooth Paste - Natural",
+        "img": " https://www.netmeds.com/images/product-v1/150x150/802119/patanjali_dant_kanti_tooth_paste_natural_200_gm_100_gm_0.jpg",
+        "price": 49,
+        "category": " Hair care ",
+        "id": "8"
+    },
+    {
+        "createdAt": "2022-11-08T12:22:24.200Z",
+        "tittle": " Truuth Adult Diaper - Strap (M) 10's",
+        "img": " https://www.netmeds.com/images/product-v1/150x150/1042880/truuth_adult_diaper_strap_m_1s_476355_0_1.jpg",
+        "price": 400,
+        "category": " Senior Care ",
+        "id": "9"
+    },
+    {
+        "createdAt": "2022-11-08T02:54:01.856Z",
+        "tittle": " Shake HandZ-E Alcohol Hand Rub 5 litre",
+        "img": " https://www.netmeds.com/images/product-v1/150x150/953905/shake_handz_e_hand_rub_5_litre_0_0.jpg",
+        "price": 157,
+        "category": " Hair care ",
+        "id": "10"
+    },
+    {
+        "createdAt": "2022-11-08T07:51:05.023Z",
+        "tittle": " Sensodyne Repair & Protect Toothpaste 100",
+        "img": " https://www.netmeds.com/images/product-v1/150x150/829099/sensodyne_repair_protect_toothpaste_100_gm_0_2.jpg",
+        "price": 61,
+        "category": "Senior Care",
+        "id": "11"
+    },
+    {
+        "createdAt": "2022-11-08T11:37:53.228Z",
+        "tittle": " Biotique Himalayan Plum Refreshing Body",
+        "img": " https://www.netmeds.com/images/product-v1/150x150/13081/biotique_himalayan_plum_refreshing_body_soap_150_gm_0_1.jpg",
+        "price": 87,
+        "category": "Oral Care",
+        "id": "12"
+    },
+    {
+        "createdAt": "2022-11-08T01:06:48.434Z",
+        "tittle": " Pee Safe Adult Diaper Extra Large High",
+        "img": " https://www.netmeds.com/images/product-v1/150x150/1019512/pee_safe_adult_diaper_extra_large_high_absorbency_pack_of_10_1s_430417_0_1.jpg",
+        "price": 68,
+        "category": "Body Care",
+        "id": "13"
+    },
+    {
+        "createdAt": "2022-11-07T16:03:59.844Z",
+        "tittle": " Comfrey Easywear Diapers Pants (XXL)",
+        "img": " https://www.netmeds.com/images/product-v1/150x150/408383/comfrey_easywear_diapers_pants_xxl_10_s_0.jpg",
+        "price": 57,
+        "category": "Oral Care",
+        "id": "14"
+    },
+    {
+        "createdAt": "2022-11-08T01:40:29.215Z",
+        "tittle": " Maxi Candy Toothbrush (Buy 1 Get",
+        "img": " https://www.netmeds.com/images/product-v1/150x150/1004100/maxi_candy_toothbrush_buy_1_get_1_free_1s_0_0.jpg",
+        "price": 51,
+        "category": "Hair care",
+        "id": "15"
+    },
+    {
+        "createdAt": "2022-11-08T10:22:03.158Z",
+        "tittle": " Insta Glow Gold Bleach 30 Gm",
+        "img": " https://www.netmeds.com/images/product-v1/150x150/944260/insta_glow_gold_bleach_30_gm_0_2.jpg",
+        "price": 46,
+        "category": "Senior Care",
+        "id": "16"
+    },
+    {
+        "createdAt": "2022-11-08T03:24:06.337Z",
+        "tittle": " Friends Adult Dry Pants - Classic (L) 10's",
+        "img": " https://www.netmeds.com/images/product-v1/150x150/924123/friends_adult_dry_pants_classic_l_10s_0_1.jpg",
+        "price": 86,
+        "category": "Body Care",
+        "id": "17"
+    },
+    {
+        "createdAt": "2022-11-07T21:21:01.952Z",
+        "tittle": " Abzorb Dusting Powder 100gm",
+        "img": " https://www.netmeds.com/images/product-v1/150x150/323943/abzorb_dusting_powder_100gm_39756_0_2.jpg",
+        "price": 1037,
+        "category": "Senior Care",
+        "id": "18"
+    },
+    {
+        "createdAt": "2022-11-08T00:14:47.649Z",
+        "tittle": " Lifree Extra Absorb Adult Diaper Pants (XL)",
+        "img": " https://www.netmeds.com/images/product-v1/150x150/1007447/lifree_extra_absorb_adult_diaper_pants_xl_10s_0_0.jpg",
+        "price": 164,
+        "category": "Senior Care",
+        "id": "19"
+    },
+    {
+        "createdAt": "2022-11-08T12:22:53.568Z",
+        "tittle": " You & Me Active Toothbrush & Tongue",
+        "img": " https://www.netmeds.com/images/product-v1/150x150/1004095/you_me_active_toothbrush_buy_1_get_1_free_0_1.jpg",
+        "price": 835,
+        "category": "Body Care",
+        "id": "20"
+    }
+]
+
+function sortfunc(){
+    let s=document.querySelector("#shift1").value
+    
+    if(s=="low"){
+        let sortdata=data.sort((a,b)=>{
+            return (a.price) - (b.price)
+        })
+        displaymain2(sortdata)
+    }else{
+        let sortdata=data.sort((a,b)=>{
+            return (b.price) - (a.price)
+        })
+        displaymain2(sortdata)
+    }
+}
+sortfunc()
+function mychange(){
+    let myda=document.querySelector("#search1").value
+    let newarr=data.filter((ele)=>{
+        return ele.category.toLowerCase().includes(myda.toLowerCase()) || ele.tittle.toLowerCase().includes(myda.toLowerCase())
+    })
+    displaymain2(newarr)
+}
+mychange()
+let cart_count=(localStorage.getItem("count"))||0
+let cartitem=JSON.parse(localStorage.getItem("cartitem")) || []
+displaymain2(data)
+function displaymain2(data){
+    document.querySelector("#main2").innerHTML=""
+    data.forEach((ele) => {
+        
+        let div=document.createElement("div")
+
+        let img=document.createElement("img")
+        img.setAttribute("src",ele.img)
+
+        let tittle=document.createElement("h2")
+        tittle.innerText=ele.tittle
+
+        let cat=document.createElement("p")
+        cat.innerText=ele.category
+
+        let price=document.createElement("h2")
+        price.innerText="Best Price : "+ele.price
+
+        let btn=document.createElement("button")
+        btn.innerText="ADD TO CART"
+        btn.addEventListener("click",function(){
+            cart_count++
+            localStorage.setItem("count",cart_count)
+            document.querySelector("#quan").textContent="🛒 Cart : "+cart_count
+
+            cartitem.push(ele)
+            localStorage.setItem("cart",JSON.stringify(cartitem))
+        })
+        
+
+        div.append(img,tittle,cat,price,btn)
+
+        document.querySelector("#main2").append(div)
+    });
+    
+
+}
